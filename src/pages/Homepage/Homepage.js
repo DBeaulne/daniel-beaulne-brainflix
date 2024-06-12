39ee925aa123fb052dbe2256ca8407759833cd98
Daniel Beaulne <38220914+DBeaulne@users.noreply.github.com>
2024-06-12 14:39:02 -0400

    initial coding for api calls with useEffect

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Header from "../../components/Header/Header";
 import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
 import MainVideo from "../../components/MainVideo/MainVideo";
@@ -8,6 +9,20 @@ import VideoData from "../../data/video-details.json";
 function Homepage() {
 	// Set initial states
 	const [mainVideo, setMainVideo] = useState(VideoData[0]);
+	const [video, setVideo] = useState([]);
+
+	useEffect(() => {
+		// Initiate axios method and initial video api call
+		const fetchInitialVideo = async () => {
+			const BaseURL = "https://unit-3-project-api-0a5620414506.herokuapp.com/";
+			const api_key = "c8f0f939-78b7-47d9-91ab-5e01f5d85ccd";
+			const response = await axios.get(`${BaseURL}videos?api_key=${api_key}`);
+			console.log(response.data);
+			console.log(response.data[0]);
+			setVideo(response.data[0]);
+		};
+		fetchInitialVideo();
+	}, []);
 
 	// function to handle the lifted state from NextVideo component
 	// when user clicks on the next video, we use the id of that video to find the corresponding index
@@ -22,7 +37,7 @@ function Homepage() {
 			<main className="mainContent">
 				<VideoPlayer image={mainVideo.image} />
 				<section className="mainVideo-container">
-					<MainVideo video={mainVideo} />
+					<MainVideo video={mainVideo} other={video} />
 					<article className="nextVideo-container">
 						<NextVideos currVideoID={mainVideo.id} onVideoSelect={handleVideoSelect} />
 					</article>
